741c07455f10eaa1471229f045cc0c6f6a7fe7d1
Emmanuel Ademola <mailemmydee@gmail.com>
2023-09-05 07:11:43 -0700

    print last digit

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    num = number
+    if num < 0:
+        num *= -1
+    last = num % 10
+
+    while num > 9:
+        last = num % 10
+        num = last
+    return (last)
