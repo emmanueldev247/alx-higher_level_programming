c1ab014dd0fc98e63c8576ddc214c782c0e89276
Emmanuel Ademola <mailemmydee@gmail.com>
2023-09-07 11:27:12 -0700

    Import script

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-if __name__ == "main":
-    from add_0 import add
+if __name__ == "__main__":
+    import add_0 as mod1
     a = 1
     b = 2
-    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} + {} = {}".format(a, b, mod1.add(a, b)))
