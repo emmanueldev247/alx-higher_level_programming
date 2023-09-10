da496c2878303da42e368aff890d676a7708737c
Emmanuel Ademola <mailemmydee@gmail.com>
2023-09-10 08:09:29 -0700

    The Matrix is revealed (mod 2)

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for x in matrix:
-        for y in x:
-            if y == x[len(x) - 1]:
-                print("{}".format(y))
-            else:
-                print("{}".format(y), end=" ")
+    if matrix == [[]]:
+        print()
+    else:
+        for x in matrix:
+            for y in x:
+                if y == x[len(x) - 1]:
+                    print("{:d}".format(y))
+                else:
+                    print("{:d}".format(y), end=" ")
