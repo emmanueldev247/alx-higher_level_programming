e6bfdc5233b701e67f5d2337b69b658fca78a92f
Emmanuel Ademola <mailemmydee@gmail.com>
2023-09-11 16:00:51 -0700

    CPython #0: Python lists (+ betty fixes)

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,7 +1,12 @@
 #include <Python.h>
 #include <listobject.h>
 #include <object.h>
-
+/**
+ * print_python_list_info - Prints information about a Python list object
+ * @p: A pointer to a Python list object
+ *
+ * Return: nothing (void)
+ */
 void print_python_list_info(PyObject *p)
 {
 	long int size, i = 0;
