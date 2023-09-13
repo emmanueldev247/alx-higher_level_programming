4b5a935e88c6518bb0c624f1287bb3faceb721f3
Emmanuel Ademola <mailemmydee@gmail.com>
2023-09-13 16:15:13 -0700

    Roman to integer (mod 4)

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,13 +2,12 @@
 def roman_to_int(roman_string):
     """Converts a Roman numeral to an integer"""
     if not roman_string.isalpha() or roman_string is None or not roman_string:
-        return None
+        return 0
 
     roman_value = {"I": 1, "V": 5, "X": 10,
                    "L": 50, "C": 100, "D": 500, "M": 1000}
-    value = 0
-    previous = 0
-    for i in reversed(roman_string):  # Start from the right and move left
+    value = previous = 0
+    for i in reversed(roman_string):
         val = roman_value.get(i, 0)
         if val < previous:
             value -= val
