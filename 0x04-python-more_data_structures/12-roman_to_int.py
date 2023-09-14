9334e1286ec9e79bf87a32e7f9368d4fb388bd10
Emmanuel Ademola <mailemmydee@gmail.com>
2023-09-14 00:06:48 -0700

    Roman to integer (mod 5)

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     """Converts a Roman numeral to an integer"""
-    if not roman_string.isalpha() or roman_string is None or not roman_string:
+    if type(roman_string) != str or not roman_string:
         return 0
 
     roman_value = {"I": 1, "V": 5, "X": 10,
