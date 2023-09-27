036b8d5a55e5ad696734201282da7614608d0c24
Emmanuel Ademola <mailemmydee@gmail.com>
2023-09-26 18:09:21 -0700

    Coordinates of a square (mod 4)

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -23,6 +23,8 @@ class Square:
             raise TypeError("position must be a tuple of 2 positive integers")
         elif (position[0] < 0) or (position[1] < 0):
             raise TypeError("position must be a tuple of 2 positive integers")
+        elif (type(value[0]) != int) or (type(value[1]) != int):
+            raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__position = position
 
