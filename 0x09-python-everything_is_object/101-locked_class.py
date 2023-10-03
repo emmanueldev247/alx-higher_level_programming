670f36ec8ec57ba490fc88d762c60abd17fc3293
Emmanuel Ademola <mailemmydee@gmail.com>
2023-10-03 12:54:46 -0700

    pycodestyle fixes

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
+"""Defined a class LockedClass with no class or object attribute"""
+
+
 class LockedClass:
+    """class that prevents the user from dynamically
+        creating new instance attributes"""
     def __setattr__(self, name, value):
+        """Special method that is called when an attempt
+            is made to set an attribute on an object"""
         if name != "first_name":
             raise AttributeError("'LockedClass' object\
  has no attribute '{}'".format(name))
