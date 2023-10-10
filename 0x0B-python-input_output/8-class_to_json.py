8df2b6bacf77b68b9c664628c35675d400721a9a
Emmanuel Ademola <mailemmydee@gmail.com>
2023-10-10 16:14:28 -0700

    Class to JSON

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -11,18 +11,3 @@ def class_to_json(obj):
         obj(class): Class to be worked on
     """
     return vars(obj)
-
-
-def append_write(filename="", text=""):
-    """function that appends a string at the end of a text file (UTF8)
-        and returns the number of characters added
-
-        Args:
-            filename(str): file to be appended to
-            text(str): string to append to "filename"
-        Returns:
-            The number of characters added
-
-    """
-    with open(filename, "a", encoding='UTF-8') as my_file:
-        return my_file.write(text)
