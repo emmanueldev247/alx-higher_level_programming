85a7e6d52d8d24326fce50bde912af8de4b8ebac
Emmanuel Ademola <mailemmydee@gmail.com>
2023-10-10 23:58:01 -0700

    Student to disk and reload (fixed)

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -9,7 +9,8 @@ class Student:
             - last_name
             - age
         Public method:
-            - def to_json(self):
+            - def to_json(self, attrs=None):
+            - def reload_from_json(self, json):
     """
 
     def __init__(self, first_name, last_name, age):
@@ -18,12 +19,27 @@ class Student:
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
+    def to_json(self, attrs=None):
         """Function that returns the dictionary description with
         simple data structure for JSON serialization of an object
+        Args:
+            attrs(list): list of strings that can contain attribute names;
+            default is None
+        Returns:
+            A dictionary representation of a Student instance
         """
-        return {
-                "first_name": self.first_name,
-                "last_name": self.last_name,
-                "age": self.age
-        }
+        if attrs is None:
+            return {key: value for key, value in self.__dict__.items()}
+        else:
+            return {k: val for k, val in self.__dict__.items() if k in attrs}
+
+    def reload_from_json(self, json):
+        """Public method that replaces all
+            attributes of the Student instance
+           Args:
+            json(dict): containing attributes to be replaced"""
+        attr_dict = dict(json)
+        print(attr_dict)
+        for key, value in attr_dict.items():
+            if key in self.__dict__:
+                self.__dict__[key] = value
