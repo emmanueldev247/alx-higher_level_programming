72ef63d3769f6169bfa9cb58ec3fe5db6d292b03
Emmanuel Ademola <mailemmydee@gmail.com>
2023-10-11 00:12:02 -0700

    removed junk file

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Create a function that reads text file"""
+
+
+def read_file(filename=""):
+    """function that reads a text file and prints it to stdout
+
+        Args:
+            filename(str): file to be read
+    """
+    with open(filename, "r", encoding='UTF-8') as my_file:
+        print(my_file.read(), end="")
