a2e5b8210b4f23ab0987ca2840ae2387c0ecc06c
Emmanuel Ademola <mailemmydee@gmail.com>
2023-10-13 04:52:33 -0700

    Base class

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""Creates a class 'Base' which happens to be
+	the base of other classes in this project"""
+
+
+class Base:
+    """The “base” of all other classes in this project"""
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Instantiation with id
+
+        Args:
+            id (int): id of class instances
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
