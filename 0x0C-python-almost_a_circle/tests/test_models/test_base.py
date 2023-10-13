82042b6a636c6eed0da707da1f9e8469464c7435
Emmanuel Ademola <mailemmydee@gmail.com>
2023-10-13 06:43:58 -0700

    Initialized test directory

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Unittest for max_integer([..])
+"""
+import unittest
+from models.base import Base
+
+class TestBases(unittest.TestCase):
+    def test_auto_id_increment(self):
+        b1 = Base()
+        b2 = Base()
+        b3 = Base()
+
+        self.assertEqual(b1.id, 1)
+        self.assertEqual(b2.id, 2)
+        self.assertEqual(b3.id, 3)
+
+    def test_custom_id(self):
+        b4 = Base(10)
+        b5 = Base(20)
+
+        self.assertEqual(b4.id, 10)
+        self.assertEqual(b5.id, 20)
+
+if __name__ == "__main__":
+    unittest.main()
