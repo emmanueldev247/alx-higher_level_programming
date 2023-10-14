be5d3da16847d429b55b607459d01fb57a77f85f
Emmanuel Ademola <mailemmydee@gmail.com>
2023-10-14 14:45:56 -0700

    rectangle

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,80 @@
+#!/usr/bin/python
+
+"""Creates a class 'Rectangle' that inherits from Base"""
+
+from models.base import Base
+
+
+class Rectangle(Base):
+    """class Rectangle that inherits from Base"""
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Instantiation with several private instance attributes
+
+        Args:
+            width(int): width of rectangle
+            height(int): height of rectangle
+            x(int):
+            y(int):
+        """
+        super().__init__(id)
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+
+
+    @property
+    def width(self):
+        """"attribute getter method"""
+        return self.__width
+
+    @width.setter
+    def width(self, width):
+        """attribute setter method"""
+        if type(width) != int:
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = width
+
+    @property
+    def height(self):
+        """"attribute getter method"""
+        return self.__height
+
+    @height.setter
+    def height(self, height):
+        """attribute setter method"""
+        if type(height) != int:
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = height
+
+    @property
+    def x(self):
+        """"attribute getter method"""
+        return self.__x
+
+    @x.setter
+    def x(self, x):
+        """attribute setter method"""
+        if type(x) != int:
+            raise TypeError("x must be an integer")
+        if x < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = x
+
+    @property
+    def y(self):
+        """"attribute getter method"""
+        return self.__y
+
+    @y.setter
+    def y(self, y):
+        """attribute setter method"""
+        if type(y) != int:
+            raise TypeError("y must be an integer")
+        if y < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = y
