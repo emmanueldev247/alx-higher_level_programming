7b1be0952e1915c7a5dda5c12e5fea2c433ebc4e
Emmanuel Ademola <mailemmydee@gmail.com>
2023-10-14 15:19:34 -0700

    Validate attributes

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -17,11 +17,29 @@ class Rectangle(Base):
             y(int):
         """
         super().__init__(id)
+        if type(width) != int:
+            raise TypeError("width must be an integer")
+        if width <= 0:
+            raise ValueError("width must be > 0")
         self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
 
+        if type(height) != int:
+            raise TypeError("height must be an integer")
+        if height <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = height
+
+        if type(x) != int:
+            raise TypeError("x must be an integer")
+        if x < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = x
+
+        if type(y) != int:
+            raise TypeError("y must be an integer")
+        if y < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = y
 
     @property
     def width(self):
@@ -33,8 +51,8 @@ class Rectangle(Base):
         """attribute setter method"""
         if type(width) != int:
             raise TypeError("width must be an integer")
-        if width < 0:
-            raise ValueError("width must be >= 0")
+        if width <= 0:
+            raise ValueError("width must be > 0")
         self.__width = width
 
     @property
@@ -47,8 +65,8 @@ class Rectangle(Base):
         """attribute setter method"""
         if type(height) != int:
             raise TypeError("height must be an integer")
-        if height < 0:
-            raise ValueError("height must be >= 0")
+        if height <= 0:
+            raise ValueError("height must be > 0")
         self.__height = height
 
     @property
