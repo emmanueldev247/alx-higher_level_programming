34c7285dc540a59165bbef1b7cfa31fe79b6ae87
Emmanuel Ademola <mailemmydee@gmail.com>
2023-10-16 20:33:07 -0700

    Square update

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -34,3 +34,45 @@ class Square(Rectangle):
         return "[Square] " + \
                "({}) {}/{} - {}".format(self.id, self.x, self.y,
                                         self.width)
+
+    def update(self, *args, **kwargs):
+        """Public method that assigns an argument to
+            attribute using *args and **kwargs
+
+           Args:
+               *args (ints): no keyword argument
+                  - 1st argument should be the id attribute
+                  - 2nd argument should be the size attribute
+                  - 3rd argument should be the x attribute
+                  - 4th argument should be the y attribute
+
+               **kwargs (dict): keyword argument
+        """
+
+        if args:
+            if len(args) >= 1:
+                self.id = args[0]
+
+            if len(args) >= 2:
+                self.size = args[1]
+
+            if len(args) >= 3:
+                self.x = args[2]
+
+            if len(args) >= 4:
+                self.y = args[3]
+        else:
+            if kwargs is not None:
+                for key, value in kwargs.items():
+
+                    if key == "id":
+                        self.id = value
+
+                    elif key == "size":
+                        self.size = value
+
+                    elif key == "x":
+                        self.x = value
+
+                    elif key == "y":
+                        self.y = value
