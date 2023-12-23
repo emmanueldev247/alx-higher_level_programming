f29afdef65a1d684146d536c3d3f57fddcc14ae4
emmanueldev247 <mailemmydee@gmail.com>
2023-12-23 10:45:41 -0800

    added: all genres of Dexter

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,8 @@
+-- Lists 14-my_genres.sql
+SELECT g.`name`
+	FROM `tv_shows` AS s
+	JOIN `tv_show_genres` AS sg
+		ON s.`id` = sg.`show_id`
+	JOIN `tv_genres` AS g
+		ON sg.`genre_id` = g.`id`
+	WHERE s.`title` = 'Dexter';
