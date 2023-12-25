d3f8c1264917bce76b73ffe279a0ebbd247f7ebc
emmanueldev247 <mailemmydee@gmail.com>
2023-12-25 15:08:28 -0800

    No comedy tonight

---
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -0,0 +1,11 @@
+-- Lists all shows without the genre Comedy in the database
+SELECT DISTINCT s.title
+FROM tv_shows AS s
+LEFT JOIN (
+    SELECT sg.show_id
+    FROM tv_genres AS g
+    JOIN tv_show_genres AS sg ON g.id = sg.genre_id
+    WHERE g.name = 'Comedy'
+) AS comedy_show ON s.id = comedy_show.show_id
+WHERE comedy_show.show_id IS NULL OR comedy_show.show_id IS NULL
+ORDER BY s.title;
