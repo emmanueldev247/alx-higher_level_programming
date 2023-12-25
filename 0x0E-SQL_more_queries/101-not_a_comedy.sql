47dac8b1dd95a3d62482279cc4c64b965abec1c8
emmanueldev247 <mailemmydee@gmail.com>
2023-12-25 15:15:04 -0800

    updated: bug fixed

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,11 +1,18 @@
 -- Lists all shws without the genre Comedy in the database
-SELECT DISTINCT s.title
-FROM tv_shows AS s
-LEFT JOIN (
-    SELECT sg.show_id
-    FROM tv_genres AS g
-    JOIN tv_show_genres AS sg ON g.id = sg.genre_id
-    WHERE g.name = 'Comedy'
-) AS comedy_show ON s.id = comedy_show.show_id
-WHERE comedy_show.show_id IS NULL OR comedy_show.show_id IS NULL
-ORDER BY s.title;
+SELECT DISTINCT `title`
+  FROM `tv_shows` AS t
+       LEFT JOIN `tv_show_genres` AS s
+       ON s.`show_id` = t.`id`
+
+       LEFT JOIN `tv_genres` AS g
+       ON g.`id` = s.`genre_id`
+       WHERE t.`title` NOT IN
+             (SELECT `title`
+                FROM `tv_shows` AS t
+	             INNER JOIN `tv_show_genres` AS s
+		     ON s.`show_id` = t.`id`
+
+		     INNER JOIN `tv_genres` AS g
+		     ON g.`id` = s.`genre_id`
+		     WHERE g.`name` = "Comedy")
+ ORDER BY `title`;
