65694818bc2388999c50201d886c73bea5024624
emmanueldev247 <mailemmydee@gmail.com>
2023-12-26 11:30:35 -0800

    updated: changed kwords to uppercase

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,7 +1,7 @@
 -- List all shows from hbtn_0d_tvshows_rate by their rating
-SELECT ts.title, SUM(tsr.rate) as rating
-FROM tv_shows as ts
-JOIN tv_show_ratings as tsr
+SELECT ts.title, SUM(tsr.rate) AS rating
+FROM tv_shows AS ts
+JOIN tv_show_ratings AS tsr
 ON ts.id = tsr.show_id
 GROUP BY ts.id
 ORDER BY rating DESC;
