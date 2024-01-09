2e5d9f4bfac90bd4d1ef50122ef4cc09c3edbfcb
emmanueldev247 <mailemmydee@gmail.com>
2024-01-08 20:42:41 -0800

    updated: index 2 not 3

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const numArgs = process.argv.length - 2;
-if (!process.argv[2]) {
+if (!process.argv[3]) {
   console.log('No argument');
 } else {
-  console.log(`${process.argv[2]}`);
+  console.log(`${process.argv[3]}`);
 }
