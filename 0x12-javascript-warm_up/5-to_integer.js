f17db44c1553ecd610354fe04f88eaa88f77f412
emmanueldev247 <mailemmydee@gmail.com>
2024-01-10 10:09:38 -0800

    updated: checks for NaN

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,3 +1,8 @@
 #!/usr/bin/node
 const arg1 = process.argv[2];
-console.log('My number: ' + parseInt(arg1));
+const argInt = parseInt(arg1);
+if (!isNaN(argInt)) {
+  console.log('My number: ' + parseInt(arg1));
+} else {
+  console.log('Not a number');
+}
