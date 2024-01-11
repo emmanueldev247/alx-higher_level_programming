c6b33bfa5d2fbfa444a95238c6542a37fd895484
emmanueldev247 <mailemmydee@gmail.com>
2024-01-11 12:33:41 -0800

    updated: "require" used

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,35 +1,7 @@
 #!/usr/bin/node
-class Rectangle {
-  constructor (w, h) {
-    if (Number.isInteger(w) && Number.isInteger(h) && w > 0 && h > 0) {
-      this.width = w;
-      this.height = h;
-    }
-  }
+const Square5 = require('./5-square');
 
-  print () {
-    for (let i = 0; i < this.height; i++) {
-      let output = '';
-      for (let j = 0; j < this.width; j++) {
-        output += 'X';
-      }
-      console.log(output);
-    }
-  }
-
-  rotate () {
-    const temp = this.width;
-    this.width = this.height;
-    this.height = temp;
-  }
-
-  double () {
-    this.width = this.width * 2;
-    this.height = this.height * 2;
-  }
-}
-
-class Square extends Rectangle {
+class Square extends Square5 {
   constructor (size) {
     super(size, size);
   }
