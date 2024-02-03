cf62c3fe8c7b7a00562017206b33bf3c9956acfc
emmanueldev247 <mailemmydee@gmail.com>
2024-02-02 16:42:19 -0800

    updated: silenced failed status code

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response
-curl -X GET $1
+curl -s -w "%{http_code}" $1 | grep -q 200 && curl -X GET $1
