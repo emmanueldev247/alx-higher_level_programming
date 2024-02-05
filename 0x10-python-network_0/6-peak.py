e087efdb680d7ad1b4503edfdfa6cc226fc1df23
emmanueldev247 <mailemmydee@gmail.com>
2024-02-05 08:42:00 -0800

    updated: factored last index

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -6,12 +6,17 @@ def find_peak(list_of_integers):
     """function that finds a peak in a list of unsorted integers"""
     if list_of_integers == []:
         return None
-    elif len(list_of_integers) == 1:
+
+    lenList = len(list_of_integers)
+
+    if lenList == 1:
         return list_of_integers[0]
 
-    for i in range(len(list_of_integers)):
-        if (list_of_integers[i] >= list_of_integers[i-1]):
-            if (list_of_integers[i] >= list_of_integers[i+1]):
-                break
-
-    return list_of_integers[i]
+    for i in range(lenList):
+        if i == lenList - 1:
+            if (list_of_integers[i] >= list_of_integers[i-1]):
+                return list_of_integers[i]
+        else:
+            if (list_of_integers[i] >= list_of_integers[i-1]):
+                if (list_of_integers[i] >= list_of_integers[i+1]):
+                    return list_of_integers[i]
