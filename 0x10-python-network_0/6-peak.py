76d9cb4064f3677d2875f58d74eec98091f93121
emmanueldev247 <mailemmydee@gmail.com>
2024-02-05 09:41:02 -0800

    fixed: methodology changed

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -12,11 +12,16 @@ def find_peak(list_of_integers):
     if lenList == 1:
         return list_of_integers[0]
 
-    for i in range(lenList):
-        if i == lenList - 1:
-            if (list_of_integers[i] >= list_of_integers[i-1]):
-                return list_of_integers[i]
-        else:
-            if (list_of_integers[i] >= list_of_integers[i-1]):
-                if (list_of_integers[i] >= list_of_integers[i+1]):
-                    return list_of_integers[i]
+    elif lenList == 2:
+        return max(list_of_integers)
+
+    mid = lenList // 2
+
+    peak = list_of_integers[mid]
+
+    if (peak >= list_of_integers[mid - 1]) and (peak >= list_of_integers[mid + 1]):
+        return peak
+    elif peak < list_of_integers[mid - 1]:
+        return find_peak(list_of_integers[:mid])
+    else:
+        return find_peak(list_of_integers[mid + 1:])
