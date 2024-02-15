dbd781b0fb583d0e2481a92fce5ec649fa68d0d0
emmanueldev247 <mailemmydee@gmail.com>
2024-02-14 20:25:44 -0800

    updated: Response header value

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -5,10 +5,12 @@ displays the value of the X-Request-Id variable"""
 import urllib.request
 import sys
 
-my_url = sys.argv[1]
-with urllib.request.urlopen(my_url) as response:
-    response_body = response.read()
 
-    headers = dict(response.headers)
-    header_val = headers.get("X-Request-Id")
-    print(header_val)
+if __name__ == "__main__":
+    my_url = sys.argv[1]
+    with urllib.request.urlopen(my_url) as response:
+        response_body = response.read()
+
+        headers = response.headers
+        header_val = headers.get("X-Request-Id")
+        print(header_val)
