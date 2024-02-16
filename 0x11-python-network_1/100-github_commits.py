b516aea2521092bd4144c3f4e530f963f90997a1
Emmanuel Ademola <mailemmydee@gmail.com>
2024-02-16 15:11:45 -0800

    added: API requests GitHub repo sha & author

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Python script that lists 10 commits (from the most recent to oldest)
+    of a GitHub repository"""
+
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    repo = sys.argv[1]
+    owner = sys.argv[2]
+
+
+    url = f"https://api.github.com/repos/{owner}/{repo}/commits"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        commits = response.json()
+
+        for x in range(10):
+            sha = commits[x]['sha']
+            author = commits[x]['commit']['author']['name']
+
+            print(f'{sha}: {author}')
