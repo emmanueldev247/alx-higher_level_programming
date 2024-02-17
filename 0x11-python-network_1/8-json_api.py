e8305a21021d19742cc6027aa7162ed437f3efb3
Emmanuel Ademola <mailemmydee@gmail.com>
2024-02-17 14:18:59 -0800

    added: Search API

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Python script that takes a letter and sends a POST request to
+    http://0.0.0.0:5000/search_user with the letter as a parameter"""
+
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    my_url = "http://0.0.0.0:5000/search_user"
+    q = sys.argv[1] if len(sys.argv) > 1 else ""
+
+    try:
+        response = requests.post(my_url, data=q).json()
+        if len(response) == 0:
+            print("No result")
+        else:
+            print(f"[{response['id']} {response['name']}")
+    except:
+        print("Not a valid JSON")
