a3f1cfbadd4f4e389b2aa93ceb32694b7a83db54
Emmanuel Ademola <mailemmydee@gmail.com>
2024-03-29 14:30:26 -0700

    updated: process.argv index

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,7 +3,7 @@
 
 const fs = require('fs');
 
-fs.readFile(process.argv[1], 'utf-8', (err, data) => {
+fs.readFile(process.argv[2], 'utf-8', (err, data) => {
   if (err) {
     console.log(err);
   } else {
