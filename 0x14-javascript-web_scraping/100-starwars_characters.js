d8dc4c98f758d147c908add93bdfeac022494bb3
Emmanuel Ademola <mailemmydee@gmail.com>
2024-04-02 00:23:48 -0700

    added: Who was playing in this movie?

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+// script that prints all characters of a Star Wars movie
+
+const request = require('request');
+
+const url = "https://swapi-api.alx-tools.com/api/films/" + process.argv[2];
+
+request.get(url, (err, response, body) => {
+  if (err) { return; }
+
+  for (const character of JSON.parse(body).characters) {
+    request.get(character, (err, response, body) => {
+      console.log(JSON.parse(body).name);
+    });
+  };
+});
