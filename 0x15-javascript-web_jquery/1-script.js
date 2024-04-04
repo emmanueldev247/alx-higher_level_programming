b81be8072af919634576bea1a62e8781a696fb2f
emmanueldev247 <mailemmydee@gmail.com>
2024-04-04 18:37:02 +0100

    updated: document readiness check

--- 0x15-javascript-web_jquery/1-script.js
+++ 0x15-javascript-web_jquery/1-script.js
@@ -1,1 +1,4 @@
-$('header').css('color', '#FF0000');
+// script that updates the text color of the <header> element to red (#FF0000)
+$(document).ready(function() {
+  $('header').css('color', '#FF0000');
+});
--- 0x15-javascript-web_jquery/2-script.js
+++ 0x15-javascript-web_jquery/2-script.js
@@ -1,8 +1,11 @@
 /* script that updates the text color of the <header> element to red (#FF0000)
    when the user clicks on the tag DIV#red_header */
 
-const redHeader = $('#red_header');
+$(document).ready(function () {
+  const redHeader = $('DIV#red_header');
+  const header = $('header');
 
-redHeader.on('click', function () {
-  redHeader.css('color', '#FF0000');
+  redHeader.on('click', function () {
+    header.css('color', '#FF0000');
+  });
 });
