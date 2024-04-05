ee79501f1f58eacf10fe90643ad9921f57df5e34
emmanueldev247 <mailemmydee@gmail.com>
2024-04-05 11:00:48 +0100

    added: Second method

--- 0x15-javascript-web_jquery/7-script.js
+++ 0x15-javascript-web_jquery/7-script.js
@@ -1,12 +1,18 @@
 /* script that fetches the character name from this URL: https://swapi-api.alx-tools.com/api/people/5/?format=json */
 
-$(document).ready(function () {
-  const character = $('DIV#character');
-  $.get('https://swapi-api.alx-tools.com/api/people/5/?format=json', function(data) {
-    const name = data.name;
-
-    character.text(name);
-   });
-
-
+const character = $('DIV#character');
+$.get('https://swapi-api.alx-tools.com/api/people/5/?format=json', function(data) {
+  const name = data.name;
+  character.text(name);
 });
+
+/* --- THIS WORKS TOO --
+$.ajax({
+    type:  'GET',
+    url: 'https://swapi-api.alx-tools.com/api/people/5/?format=json',
+    success: function(data) {
+        const name = data.name;
+        character.text(name);
+    }
+ });
+ */
