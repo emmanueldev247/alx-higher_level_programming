73e887b4e288fd669816864305b9a30edcf4e39a
Emmanuel Ademola <mailemmydee@gmail.com>
2024-04-05 04:36:41 -0700

    updated: semistandard compliance

--- 0x15-javascript-web_jquery/100-script.js
+++ 0x15-javascript-web_jquery/100-script.js
@@ -1,3 +1,3 @@
-document.addEventListener('DOMContentLoaded', ()=> {
+document.addEventListener('DOMContentLoaded', () => {
   document.querySelector('header').style.color = '#FF0000';
 });
--- 0x15-javascript-web_jquery/101-script.js
+++ 0x15-javascript-web_jquery/101-script.js
@@ -1,21 +1,21 @@
 /* script that adds, removes and clears LI elements from a list when the user clicks */
 
-$(document).ready( ()=> {
-  const item = "<li>Item</li>";
+$(document).ready(() => {
+  const item = '<li>Item</li>';
   const myList = $('UL.my_list');
-  const addItem =$('DIV#add_item');
-  const removeItem =$('DIV#remove_item');
+  const addItem = $('DIV#add_item');
+  const removeItem = $('DIV#remove_item');
   const clearList = $('DIV#clear_list');
 
-  addItem.click(()=> {
+  addItem.click(() => {
     myList.append(item);
   });
 
-  removeItem.click(()=> {
+  removeItem.click(() => {
     myList.children().last().remove();
   });
 
-  clearList.click(()=> {
+  clearList.click(() => {
     myList.empty();
   });
 });
