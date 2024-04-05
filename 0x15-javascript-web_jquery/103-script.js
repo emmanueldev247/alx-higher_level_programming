a65c9455a4ee8f35cc775e163ad176af0911fd7a
emmanueldev247 <mailemmydee@gmail.com>
2024-04-05 15:36:17 +0100

    added: And press ENTER

---
+++ 0x15-javascript-web_jquery/103-script.js
@@ -0,0 +1,28 @@
+/* script that fetches and prints how to say “Hello” depending on the language */
+
+$(document).ready(() => {
+  const translateBtn = $('INPUT#btn_translate');
+  const helloDiv = $('DIV#hello');
+    const langBox = $('INPUT#language_code');
+
+  translateBtn.click(helloAPI);
+
+  translateBtn.keypress(function(event) {
+    if (event.keyCode == 13) {
+      helloAPI;
+    }
+  });
+
+  function helloAPI() {
+    console.log('called');
+    const url = 'https://hellosalut.stefanbohacek.dev/?lang=' + langBox.val().trim();
+    $.ajax({
+      type: 'GET',
+      dataType: 'json',
+      url: url,
+      success: function (data) {
+        helloDiv.html('<span>' + data.hello + '</span>');
+      }
+    });
+  }
+});
