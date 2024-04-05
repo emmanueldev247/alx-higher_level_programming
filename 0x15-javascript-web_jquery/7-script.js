aad0703866a261455e9c5e802703ff11105ad3c5
Emmanuel Ademola <mailemmydee@gmail.com>
2024-04-05 03:08:41 -0700

    updated: semistandard compliance

--- 0x15-javascript-web_jquery/7-script.js
+++ 0x15-javascript-web_jquery/7-script.js
@@ -1,7 +1,7 @@
 /* script that fetches the character name from this URL: https://swapi-api.alx-tools.com/api/people/5/?format=json */
 
 const character = $('DIV#character');
-$.get('https://swapi-api.alx-tools.com/api/people/5/?format=json', function(data) {
+$.get('https://swapi-api.alx-tools.com/api/people/5/?format=json', function (data) {
   const name = data.name;
   character.text(name);
 });
